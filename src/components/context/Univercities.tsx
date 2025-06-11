d14ba9151479ba2a5bfad7ad8d95dc8d48e0633f
hyunwoo <hyunwoo@aah-e.net>
2025-06-11 17:35:53 +0200

    Added univercity list.

--- src/components/context/Univercities.tsx
+++ src/components/context/Univercities.tsx
@@ -1,6 +1,8 @@
 import { getTranslation } from '@/lib/i18n.server';
 import ParallaxImage from '@common/parallaxImage';
 
+import { univercities } from '@/lib/univercities';
+
 export default async function Univercities() {
   const { t } = await getTranslation();
 
@@ -11,49 +13,35 @@ export default async function Univercities() {
       <div className="relative z-10 bg-gradient-to-b from-[#f3e1eb] to-[#fff] w-full px-6 py-12">
         <div className="max-w-7xl mx-auto">
           <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">{t('UNI_TITLE')}</h2>
+
           <div className="text-lg text-gray-800 ">
-            <p className="mb-5">
-              If you are thinking about studying abroad in Korea, our Japanese staff with experience
-              studying abroad will personally and personally help you with all the procedures for
-              studying abroad, such as choosing a school, various planning, and application, as well
-              as accommodation in Korea, local procedures, and student life.
-            </p>
-            <p>
-              <strong>We are</strong>
-            </p>
-            <ul className="list-disc pl-12 ml-0">
-              <li>
-                a trusted company with <strong>15 years of experience</strong>, specializing in{' '}
-                <strong>study abroad programs in Korea.</strong>
-              </li>
-              <li>
-                officially authorized and registered as{' '}
-                <strong>an education-related organization in both Japan and Korea</strong> — a rare
-                and reliable distinction in the industry.
-              </li>
-              <li>
-                a regular member of the NPO Study Abroad Association and designated as part of the
-                Korean Secretariat.
-              </li>
-              <li>
-                one of the largest study abroad agency, sending 700 students from Japan to Korea
-                every year.
-              </li>
-              <li>
-                partnered with numerous university training institutions across Korea, building
-                strong and lasting collaborations.
-              </li>
-              <li>
-                capable of providing a wide range of study abroad options, including 3-week
-                short-term programs, 10-week regular programs, full undergraduate and graduate
-                degrees, and cyber university programs.
-              </li>
-              <li>
-                fully equipped to support students with school and dormitory arrangements,
-                documentation and visa guidance, local SIM card setup, insurance, and other on-site
-                preparations.
-              </li>
-            </ul>
+            {/* body */}
+            <p className="mb-5">{t('UNI_DESCRIPTION')}</p>
+
+            <div className="mt-10">
+              <ul
+                className="list-none grid 
+               grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
+               gap-x-8 gap-y-4 
+               w-fit mx-auto"
+              >
+                {univercities.map((item, index) => (
+                  <li key={index} className="flex justify-start items-center gap-4 py-3">
+                    <img
+                      src={`/assets/emblem/${item.emblem}.png`}
+                      alt={item.emblem}
+                      className={`w-${item.width ?? 12} h-${item.height ?? 12} ${item.margin ? 'ml-1.5 mr-1.5' : ''}`}
+                    />
+                    <strong className="flex flex-col">
+                      {item.title}
+                      <span className="text-sm text-gray-500 font-normal">{item.subtitle}</span>
+                    </strong>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* ./body */}
           </div>
         </div>
       </div>
