98784cf80a86e046749105d029713568857018f3
hyunwoo <hyunwoo@aah-e.net>
2025-07-04 08:16:29 +0200

    Set default content type.

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 // Axios 기본 설정
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default axios;
 
