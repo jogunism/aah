a43c3cddd64609e275209a383578f5ccc20e0ef5
hyunwoo <hyunwoo@aah-e.net>
2025-07-17 17:25:31 +0200

    Added link

--- src/components/context/Footer/index.tsx
+++ src/components/context/Footer/index.tsx
@@ -1,6 +1,26 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-[#DF7B7B] text-center p-6 border-t border-gray-300">
+    <footer className="w-full bg-[#DF7B7B] text-center px-6 py-4 border-t border-gray-300">
+      <div className="mb-2">
+        {/* Added margin-top for spacing */}
+        <a
+          href="https://www.instagram.com/cloverleaf0827/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-200 text-sm"
+        >
+          Instagram
+        </a>
+        <span className="mx-2 text-white">|</span> {/* Separator */}
+        <a
+          href="https://aah-e.net/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-200 text-sm"
+        >
+          aah-e.net
+        </a>
+      </div>
       <p className="text-gray-800 text-sm text-white">
         © 2025 aah! education Europe. All rights reserved.
       </p>
