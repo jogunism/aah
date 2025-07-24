7bae8e501f97adb742f313e886f2c47675e2bf57
hyunwoo <hyunwoo@aah-e.net>
2025-07-24 14:02:15 +0200

    Solved compile errors.

--- src/lib/useClientTranslation.ts
+++ src/lib/useClientTranslation.ts
@@ -1,12 +1,12 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useTranslation, UseTranslationOptions } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { i18nPromise } from './i18n.client';
 
-export const useClientTranslation = (options?: UseTranslationOptions<any>) => {
+export const useClientTranslation = () => {
   const [ready, setReady] = useState(false);
-  const translation = useTranslation(undefined, options);
+  const translation = useTranslation();
 
   useEffect(() => {
     i18nPromise.then(() => {
